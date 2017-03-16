3940585aa4ad0469231e28cc9ca1e98a1c98efba
Pezhman Nasirifard <epezhman@gmail.com>
2017-03-16 17:11:19 +0100

    'params'

--- demand_manager_admin/settings.py
+++ demand_manager_admin/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secrets['secretKey']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['131.159.52.146', '127.0.0.1']
 
--- static/temp.js
+++ static/temp.js
@@ -1,3 +1,4 @@
 /**
  * Created by epezh on 16.03.2017.
  */
+//uwsgi --http :8000 --module demand_manager_admin.wsgi
